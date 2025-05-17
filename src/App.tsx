0e8d45ef68e79ad8197a7207e4b323faf00a0c78
shehwar-dev <shehwar.dev@gmail.com>
2025-05-17 15:26:03 +0500

    feat: react routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import { AppRoutes } from "./router/AppRoutes";
 
-function App() {
+export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <AppRoutes />
     </>
   );
 }
-
-export default App;
---
+++ src/router/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+
+export const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
+};
